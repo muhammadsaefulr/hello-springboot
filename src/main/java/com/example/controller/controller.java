7ce1764426a50026a73687f0c33e1e25b699f445
muhammadsaefulr <saepull.id@gmail.com>
2023-10-16 13:17:54 +0700

    upload

---
+++ src/main/java/com/example/controller/controller.java
@@ -0,0 +1,15 @@
+
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @RequestMapping("/hello")
+    String hello() {
+        return "Hello World";
+    }
+
+}
